50f1ea8adafa1525b6e4e225e081ffb675a4a8d4
한병찬 <gks5927@mediazen.co.kr>
2023-03-02 17:42:06 +0900

    현 위치에서 찾기 버튼 추가, 클릭시 모든 마커 제거

--- src/pages/mapseeker.tsx
+++ src/pages/mapseeker.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 /*global kakao*/
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/mapseeker.module.scss";
 
 export default function mapseeker() {
   const mapRef = useRef(null);
+  const markers: any[] = [];
 
   useEffect(() => {
     if (!mapRef.current) return;
@@ -23,14 +25,22 @@ export default function mapseeker() {
     let places = new kakao.maps.services.Places();
     let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 
+    const imageSrc =
+      "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+    // 마커 이미지의 이미지 크기 입니다
+    const imageSize = new kakao.maps.Size(24, 35);
+
+    // 마커 이미지를 생성합니다
+    const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
     function placesSearchCB(data: string | any[], status: any) {
       if (status === kakao.maps.services.Status.OK) {
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
-        var bounds = new kakao.maps.LatLngBounds();
+        let bounds = new kakao.maps.LatLngBounds();
 
-        for (var i = 0; i < data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
           displayPlace(data[i]);
           bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }
@@ -42,11 +52,11 @@ export default function mapseeker() {
     // 지도에 마커를 표시하는 함수입니다
     function displayPlace(place: { y: any; x: any; place_name: string }) {
       // 마커를 생성하고 지도에 표시합니다
-      var marker = new kakao.maps.Marker({
+      let marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x),
       });
-
+      markers.push(marker);
       // 마커에 클릭이벤트를 등록합니다
       kakao.maps.event.addListener(marker, "click", function () {
         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
@@ -64,11 +74,9 @@ export default function mapseeker() {
         let lat = position.coords.latitude; // 위도
         let lon = position.coords.longitude; // 경도
         let locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-        let message =
-          '<div style="position: relative; line-height : 30px; left: 50%;">현재 위치</div>'; // 인포윈도우에 표시될 내용입니다
 
         // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
+        displayMarker(locPosition);
         geocoder.coord2Address(
           locPosition.getLng(),
           locPosition.getLat(),
@@ -87,34 +95,18 @@ export default function mapseeker() {
           }
         );
       });
-    } else {
-      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-
-      let locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
-        message = "위치를 찾을 수 없어요";
-
-      displayMarker(locPosition, message);
     }
 
     // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-    function displayMarker(locPosition: any, message?: string) {
+    function displayMarker(locPosition: any) {
       // 마커를 생성합니다
       let marker = new kakao.maps.Marker({
         map: map,
         position: locPosition,
+        image: markerImage,
       });
 
-      let iwContent = message; // 인포윈도우에 표시할 내용
-      let iwRemoveable = false;
-
-      // 인포윈도우를 생성합니다
-      let infowindow = new kakao.maps.InfoWindow({
-        content: iwContent,
-        removable: iwRemoveable,
-      });
-
-      // 인포윈도우를 마커위에 표시합니다
-      infowindow.open(map, marker);
+      markers.push(marker);
 
       // 지도 중심좌표를 접속위치로 변경합니다
       map.setCenter(locPosition);
@@ -123,14 +115,31 @@ export default function mapseeker() {
     return () => {
       kakao.maps.event.removeListener(map, "idle");
     };
-  }, [mapRef]);
+  }, [mapRef, markers]);
 
+  const onHandleClick = () => {
+    removeMarkers();
+  };
+  const removeMarkers = () => {
+    markers.forEach((value) => {
+      value.setMap(null);
+    });
+  };
   return (
     <div>
       <section>
         <h2 className={styles.h2}>내 주변 맛집</h2>
       </section>
-      <div className={styles.map} ref={mapRef}></div>
+      <div id="map" ref={mapRef}></div>
+      <button className={styles.reset} onClick={onHandleClick}>
+        현 위치에서 찾기
+      </button>
+      <style jsx>{`
+        #map {
+          width: 100vw;
+          height: 80vh;
+        }
+      `}</style>
     </div>
   );
 }
